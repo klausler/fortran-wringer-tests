0cd729a573eb62bc6ff14442590cde000cdd5f95
Peter Klausler <pklausler@nvidia.com>
2023-06-29 13:16:01 -0700

    add comment to namelist-host-assoc.f90

--- namelist-host-assoc.f90
+++ namelist-host-assoc.f90
@@ -1,3 +1,9 @@
+! Does a name whose first appearance in a scope is as part of a
+! NAMELIST group resolve to a local declaration or to a host
+! association?
+! GNU, NVF: host
+! Intel, XLF, f18: local
+! NAG: error
 program main
   real :: x = 3.14159
   call subr
