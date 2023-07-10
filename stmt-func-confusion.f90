c6c62a9015a0f4209d4d970804da25a20edef5d1
Peter Klausler <pklausler@nvidia.com>
2023-07-10 14:57:20 -0700

    add stmt-func-confusion.f90

---
+++ stmt-func-confusion.f90
@@ -0,0 +1,26 @@
+! There's a syntactic ambiguity between a statement function definition,
+! an assignment to an element of a whole array, and an assignment to the
+! target of a pointer-value function.
+! GNU: prints 0.
+! Intel: prints 1,
+! NVF, NAG, XLF: syntax errors
+! f18: assignment to target of pointer-valued function not yet implemented in lowering
+module m
+  real, target :: x = 1.
+ contains
+  function rpf(x)
+    real, intent(in out), target :: x
+    real, pointer :: rpf
+    rpf => x
+  end
+  subroutine test
+    ! Assignment to pointer-valued function, *not* a statement
+    ! function.
+    rpf(x) = 2.
+    print *, x
+  end
+end
+
+use m
+call test
+end
