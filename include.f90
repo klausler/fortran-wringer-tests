6bcffc430803c3baa1181b9c0340e1f7f5a4d66a
Peter Klausler <pklausler@nvidia.com>
2023-07-11 14:02:28 -0700

    include.f90

---
+++ include.f90
@@ -0,0 +1,4 @@
+! Ever since Fortran '90, INCLUDE lines have been allowed to have a numeric
+! kind prefix on their file names.  No compiler that I can find supports
+! this feature, other than f18.
+include 1_"inc"
