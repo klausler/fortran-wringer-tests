fcbdb628cdf38b42195bd0b6bad6ec8a3decc1a2
Peter Klausler <pklausler@nvidia.com>
2023-10-31 16:36:28 -0700

    add polymorphic-actual.f90

---
+++ polymorphic-actual.f90
@@ -0,0 +1,37 @@
+! GNU, NAG: bogus compilation errors
+! Intel, NVF: incorrect results (no temp used?)
+! XLF: works correctly
+module m
+  type t
+    integer m
+  end type
+  type, extends(t) :: t2
+    integer n
+  end type
+ contains
+  subroutine passToImplicit(a, n)
+    integer :: n
+    class(t) :: a(n)
+    external implicitInterface
+    call implicitInterface(a, n)
+  end
+end
+
+subroutine implicitInterface(a, n)
+  use m
+  integer :: n
+  type(t) a(n)
+  print *, 'got: ', a
+  a%m = a%m * 2
+end
+
+program test
+  use m
+  integer, parameter :: n = 3
+  type(t2) a(n)
+  a = [t2(1,2), t2(3,4), t2(5,6)]
+  print *, 'before: ', a
+  call passToImplicit(a, n)
+  print *, 'after: ', a
+  print *, 'expect: 2 2 6 4 10 6'
+end
