66810a894a3631512866643d8360545ca6d9f418
Peter Klausler <pklausler@nvidia.com>
2023-11-29 11:09:02 -0800

    add no-iface-procedure.f90

---
+++ no-iface-procedure.f90
@@ -0,0 +1,19 @@
+! GNU, Intel, f18: works
+! NVF, NAG, XLF: various errors
+module m
+ contains
+  subroutine test
+    procedure(), pointer :: p0
+    procedure(f), pointer :: pf
+    p0 => f
+    pf => p0
+    print *, pf(123)
+  end
+  integer function f(j)
+    f = j
+  end
+end
+
+use m
+call test
+end
