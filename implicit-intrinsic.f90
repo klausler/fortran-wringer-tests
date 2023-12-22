58d67ed9d947370f209df8ced9f9ba4303e7d0a4
Peter Klausler <pklausler@nvidia.com>
2023-12-21 17:02:49 -0800

    add implicit-intrinsic.f90

---
+++ implicit-intrinsic.f90
@@ -0,0 +1,20 @@
+! If an intrinsic function does not appear in an explicit INTRINSIC
+! statement in a module's specification part, but is referenced in
+! a declaration or initialization, is it still considered to be part
+! of the module to be USE-associated into other scopes?
+! GNU, NVF, f18: no
+! Intel, NAG, XLF: yes
+module explicit
+  intrinsic cos
+end
+subroutine testExplicit
+  use explicit
+  real :: cos = 2. ! must be an error
+end
+module implicit
+  real :: one = cos(0.)
+end
+subroutine testImplicit
+  use implicit
+  real :: cos = 2. ! may or may not be an error
+end
